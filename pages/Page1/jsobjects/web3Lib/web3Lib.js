79127cdbb711704554afe7e03bcc9863983fb843
mtest1 <mtest1@mpl.io>
2024-03-18 09:16:36 +0000

    Update code read SC

--- pages/Page1/jsobjects/web3Lib/web3Lib.js
+++ pages/Page1/jsobjects/web3Lib/web3Lib.js
@@ -1,3 +1,17 @@
 export default {
-	rpc: "https://rpc.ankr.com/polygon_mumbai",
+	rpc: "https://polygon-mumbai-bor-rpc.publicnode.com",
+	abi: [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"sender","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"},{"indexed":false,"internalType":"string","name":"name","type":"string"}],"name":"ValuesSet","type":"event"},{"inputs":[],"name":"getAmount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getName","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_amount","type":"uint256"},{"internalType":"string","name":"_name","type":"string"}],"name":"setValues","outputs":[],"stateMutability":"nonpayable","type":"function"}],
+	async getName() {
+		const contractAddr = "0xcAd1a06751Ad03FCDA2Ee4231b1d7223C972F34D";
+		
+		const web3 = new Web3(web3Lib.rpc);
+		const contract = new web3.eth.Contract(web3Lib.abi, contractAddr);
+		console.log(web3Lib.abi);
+		let res = '';
+	
+				res = await contract.methods.getName().call();
+			  console.log("aaa", res);
+		
+		return res;
+	},
 }
